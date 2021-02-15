ac4c537a35a8c2be430c77b4da3f46e962f49282
naka-prog <koji773855@ruby.plala.or.jp>
2021-02-15 10:12:21 +0900

    Initial Commit

---
+++ Arithmetic7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Arithmetic7
+//
+//  Created by koji nakashima on 2021/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
