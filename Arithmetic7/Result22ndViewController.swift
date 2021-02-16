0938fd8d17d2debd79afdf6c754eae15ee9353ef
naka-prog <koji773855@ruby.plala.or.jp>
2021-02-16 11:10:49 +0900

    メソッド追加

---
+++ Arithmetic7/Result22ndViewController.swift
@@ -0,0 +1,44 @@
+//
+//  Result22ndViewController.swift
+//  Arithmetic5
+//
+//  Created by koji nakashima on 2021/01/30.
+//
+
+import UIKit
+
+class Result22ndViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+
+    var result:[String] = []
+
+    // 戻るボタン
+    @IBAction func modoruButton1(_ sender: Any) {
+        // ２階層前の画面に戻る
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
+
+    }
+
+    @IBOutlet weak var result22ndView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        result22ndView.dataSource = self
+        result22ndView.delegate = self
+        print("Quiz → \(result)")
+    }
+
+    // テーブルの行数を指定するメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return result.count
+    }
+    
+    // セルの中身を設定するメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier:"Mycell", for:indexPath)
+        // セルに値を設定する
+        cell.textLabel!.text = result[indexPath.row]
+        cell.textLabel?.font = UIFont(name:"Menlo", size: 20)
+        return cell
+    }
+}
